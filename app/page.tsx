107021d9149190adff16923dcfa36e0df6c9c0da
langford <langfordquarshie21@gmail.com>
2025-10-13 10:07:27 +0000

    style: update landing

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,31 @@ import Link from "next/link";
 
 export default function Home() {
   return (
-    <main className="h-screen flex items-center gap-4 justify-center">
-      {PAGES.map((page) => (
-        <Link href={page.route} key={page.route}>
-          <Card className="size-[300px]">
-            <CardContent className="text-center flex items-center justify-center h-full">
-              <p>{page.label}</p>
-            </CardContent>
-          </Card>
-        </Link>
-      ))}
-    </main>
+    <div className="flex items-center justify-center">
+      <div className="max-w-[800px] space-y-10 px-10 pt-32">
+        <div>
+          <h1 className="text-xl md:text-[3rem] font-medium text-left">
+            A free Instagram photo splitter and collage maker
+          </h1>
+        </div>
+        <div className="space-y-4">
+          <p className="text-xl text-neutral-500">What do you want to do?</p>
+          <div className="grid md:grid-cols-3 gap-4">
+            {PAGES.map((page) => (
+              <Link href={page.route} key={page.route}>
+                <Card
+                  className={`h-[100px] w-full ${page.enabled === false ? "opacity-40 cursor-not-allowed" : ""}`}
+                >
+                  <CardContent className="text-center text-neutral-600 gap-2 flex items-center justify-center h-full">
+                    <page.icon className="text-neutral-300" size={20} />
+                    <p>{page.description}</p>
+                  </CardContent>
+                </Card>
+              </Link>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
--- lib/contants.ts
+++ lib/contants.ts
@@ -1,15 +1,24 @@
+import { Clock, Grid, Scissors } from "lucide-react";
+
 export const PAGES = [
   {
     label: "Collage",
+    description: "Create a collage",
     route: "/collage",
+    icon: Grid,
   },
   {
     label: "Splitter",
     route: "/splitter",
+    icon: Scissors,
+    description: "Split an image",
   },
   {
     label: "Scheduler",
     route: "#",
+    icon: Clock,
+    description: "Schedule a post",
+    enabled: false,
   },
 ];
 
